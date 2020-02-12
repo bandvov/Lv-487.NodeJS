eac28b5e7d18cc500f13b73d2de0038d7a47f613
bandvov <48312647+bandvov@users.noreply.github.com>
2020-02-12 11:28:32 +0200

    560 fixed

--- src/tasks/tasks_vovav.js
+++ src/tasks/tasks_vovav.js
@@ -45,40 +45,39 @@ function findPrime(n) {
 
 // 560
 
-function findRange(x, y) {
-        const findDeviders = n => {
-                if (typeof n !== 'number' || n === '') {
-                        throw new Error('argument must be an integer');
-                }
-                const deviders = [];
-                for (let i = 0; i < n; i++) {
-                        if (n % i === 0) {
-                                deviders.push(i);
-                        }
-                }
-                return deviders;
-        };
-        const obj = {};
-        const sumObj = {};
-        const resultObj = {};
-        for (let i = x; i <= y; i++) {
-                obj[i] = findDeviders(i);
-        }
-        for (const key in obj) {
-                let res = 0;
-                obj[key].forEach(item => {
-                        res += item;
-                });
-                sumObj[key] = res;
-        }
-        for (let i = 200; i <= 300; i++) {
-                for (const key in sumObj) {
-                        if (i === sumObj[key]) {
-                                resultObj[key] = sumObj[key];
-                        }
-                }
-        }
-        return resultObj;
+function  findRange(x, y) {
+  const findDeviders = n => {
+    if (typeof n !== "number" || n === "") {
+      throw new Error("argument must be an integer");
+    }
+    let dividers = [];
+    for (let i = 0; i < n; i++) {
+      if (n % i === 0) {
+        dividers.push(i);
+      }
+    }
+    return dividers;
+  };
+
+  let obj = {};
+  let sumObj = {};
+  let resultObj = {};
+
+  for (let i = x; i <= y; i++) {
+    obj[i] = findDeviders(i);
+  }
+  for (let key in obj) {
+    sumObj[key] = obj[key].reduce((sum, elem) => sum + elem);
+  }
+  let tempObj = { ...sumObj };
+  for (let key in sumObj) {
+    for (let item in tempObj)
+      if (key == tempObj[item] && sumObj[key] == item) {
+        resultObj[key] = item;
+      }
+  }
+ 
+  return resultObj;
 }
 tasks.set(
         '182',
